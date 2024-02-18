d82ff06dccd408afc2754d951f6f43ffba362b9d
Goz <goz@goz-mbp15.goz>
2024-02-18 18:25:00 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Embiggen Google Docs Cursor
+
+This extension will make the thin Google Docs cursor a lot thicker. Makes it easier to find.
---
+++ egdc.js
@@ -0,0 +1,4 @@
+var cursor = document.getElementsByClassName("kix-cursor-caret")[0]
+var cursor_style = cursor.getAttribute("style");
+cursor.setAttribute('style', cursor_style+" border-left: 10px solid;");
+
